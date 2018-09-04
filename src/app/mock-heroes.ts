6cee2229c631d8631387ab14b9c7aba460e09a87
kbrzeczek <kbrzeczek@ITEM-S44111>
2018-09-04 16:52:13 +0200

    Hero list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./Hero";
+
+export const HEROES: Hero[]= [
+{id: 12, name: 'Hyzio'},
+{id: 13, name: 'Zyzio'},
+{id: 14, name: 'Dyzio'},
+{id: 15, name: 'Humty'},
+{id: 16, name: 'Dumpty'},
+
+];
