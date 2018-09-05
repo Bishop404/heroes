f901d460946240116a446ae7344eeaa2ba46ca64
kbrzeczek <kbrzeczek@ITEM-S44111>
2018-09-05 14:33:30 +0200

    navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,4 +18,8 @@ export class HeroService {
   getMessage(str: string): void {
     this.messageService.add(str);
   }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
